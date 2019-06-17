23d0b782ba62c002d5252ba3599b6a36d1facd38
Balintataw J <balintataw2@gmail.com>
2019-06-16 17:01:14 -0700

    Initial

---
+++ routes/contacts.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+// @route  GET api/contacts
+// @desc   Get all users contacts
+// @access Private
+router.get('/', (req, res) => {
+    res.send('Get a users contacts');
+});
+
+// @route  POST api/contacts
+// @desc   Add new contact
+// @access Public
+router.post('/', (req, res) => {
+    res.send('Create a new contact');
+});
+
+// @route  PUT api/contacts/:id
+// @desc   Update contact
+// @access Public
+router.put('/:id', (req, res) => {
+    res.send('Update a  contact');
+});
+
+// @route  DELETE api/contacts/:id
+// @desc   Update contact
+// @access Public
+router.delete('/:id', (req, res) => {
+    res.send('Delete a  contact');
+});
+module.exports = router;
