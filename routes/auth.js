33cc1ee7822dd441fdc01c8719a0c0b44c486fcf
Balintataw J <balintataw2@gmail.com>
2019-06-16 20:18:02 -0700

    Created user auth and user create route and auth middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,72 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const router = express.Router();
+
+const authMiddleware = require('../middleware/auth');
+const User = require('../models/User');
 
 // @route  GET api/auth
 // @desc   Get logged in user
 // @access Private
-router.get('/', (req, res) => {
-    res.send('Get logged in user');
+router.get('/', authMiddleware, async (req, res) => {
+    // req.user comes from authMiddleware
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.status(200).json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route  POST api/auth
 // @desc   Auth user and get token
 // @access Public
-router.post('/', (req, res) => {
-    res.send('Log in user');
-});
+router.post(
+    '/',
+    [
+        check('email', 'Email is invalid').isEmail(),
+        check('password', 'Password is required'),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { email, password } = req.body;
+        try {
+            let user = await User.findOne({ email });
+            if (!user) {
+                res.status(400).json({ msg: 'Invalid credentials' });
+            }
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                res.status(400).json({ msg: 'Invalid credentials' });
+            }
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
+            jwt.sign(
+                payload,
+                config.get('jwt-secret'),
+                {
+                    expiresIn: 3600,
+                },
+                (err, token) => {
+                    if (err) throw new Error('Error setting token');
+                    res.status(200).json({ token });
+                }
+            );
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).json({ msg: err.message });
+        }
+    }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const connect = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-    res.json({ hey: 'Welcome to ContactKeeper API' });
-});
+// mongo connection
+connect();
+
+// Init middleware
+app.use(express.json({ extended: false }));
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+app.get('/', (req, res, next) => {
+    res.json({ hey: 'Welcome to ContactKeeper API' });
+});
 
 const PORT = process.env.PORT || 5000;
 
