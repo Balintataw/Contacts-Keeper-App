237431f249d092908477d5d925505038691a9bd1
Balintataw J <balintataw2@gmail.com>
2019-06-19 12:31:41 -0700

    Fixed bug in express static assets

--- server.js
+++ server.js
@@ -15,12 +15,13 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder in /client/build
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', index.html));
-    });
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
 }
 
 app.get('/', (req, res, next) => {
